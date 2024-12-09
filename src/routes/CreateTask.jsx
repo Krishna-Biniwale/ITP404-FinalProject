ef02d4009755d5c0abf8d1a8f06f214f2f35f7f6
Krishna Biniwale <kbiniwal@usc.edu>
2024-12-08 20:31:49 -0800

    Final project

---
+++ src/routes/CreateTask.jsx
@@ -0,0 +1,27 @@
+import { useNavigate } from "react-router-dom";
+import TaskForm from "../TaskForm";
+import { toast } from "react-toastify";
+import { useEffect } from "react";
+
+export default function CreateTask() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    document.title = "Create New Task";
+  },);
+
+  return (
+    <TaskForm onSubmit={(newTask) => {
+        fetch(`/tasks`, {
+          method: "POST",
+          body: JSON.stringify(newTask),
+          headers: {
+            "Content-type": "application/json",
+          },
+        }).then(() => {
+          navigate(`/tasks/${newTask.id}`);
+          toast.success("Task created successfully.");
+        });
+      }}
+    />
+  );
+}
